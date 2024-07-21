768817549503cf12da255270cc5ad38dfbdd9b74
Sierraplay117 <homarsierra98@gmail.com>
2024-07-20 22:15:32 -0500

    Add files via upload

---
+++ Comando de chistes magicos.php
@@ -0,0 +1,29 @@
+
+<?php
+    $respuestas = array(
+        "Si se muere una pulga, ¿a dónde va? Al pulgatorio.",
+        "Sí los zombies se deshacen con el paso del tiempo ¿zombiodegradables?",
+        "¿Cómo se llaman los hijos de Bruce Lee? Broco Lee y Ajonjo Lee.",
+        "¿Me da un café con leche corto? Se ha roto la máquina, cambio.",
+        "¿Cómo se queda un mago después de comer? Magordito.",
+        "¿Por qué el libro de matemáticas se contravivio? Porque tenía demasiados problemas.",
+        "¿Cuál es el colmo de Batman? Que le Robin.",
+	"¿Quién es el papá del príncipe azul? El Blu-ray.",
+	"Los hippies no orinan: hacen peace.",
+	"¿Cómo se llama un bumerán que no vuelve? Palo.",
+	"En Hawái no te hospedan: te alohan.",
+	"porque los changos no van a las fiestas? Porque son pachangas",
+	"Si un abogado está loco... ¿pierde el juicio?",
+	"Doctor, soy asmático, ¿es grave? No amigo, es esdrújula. - chiste educativo",
+	"¿Cuál es el idioma de las tortugas? El tortugués.",
+	"Ayer me caí y pensé que me había roto el peroné. Peronó.",
+	"¿Qué es un pelo en una cama? El bello durmiente.",
+	"Iba a contar un chiste sobre sodio... pero Na. - chiste educativo",
+	"Pues me acabo de leer El Principito. No seas inculto, se dice prólogo. - chiste educativo",
+	"¿Cuál es el colmo de un astronauta? Que enferme de gravedad.",
+	"¿Dónde cuelga Superman su supercapa? En superchero.",
+	"¿Por que Superman siempre huele bien? Por superfume."
+        );
+    $random = array_rand($respuestas);
+    echo $respuestas[$random];
+    ?>
